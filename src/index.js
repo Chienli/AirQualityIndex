be0e50719db28294638cd4e9f84fe4bc0db563a7
Chienli <xup6ru03xu4@gmail.com>
2019-02-19 04:51:36 +0800

    done

--- src/index.js
+++ src/index.js
@@ -1,49 +1,123 @@
 import "./styles.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import fetchJsonp from "fetch-jsonp";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       data: {},
-      isLoading: true
+      isLoading: true,
+      cityName: "高雄市",
+      publish: "1997-01-09 08:00",
+      casedata: {
+        aqi: [],
+        status: [],
+        siteName: [],
+        cityBeChoosed: ""
+      },
+      statusData: {
+        siteName: "前金",
+        aqi: 156,
+        O3: "28",
+        PM10: "13",
+        PM25: "6",
+        CO: "0.24",
+        SO2: "1.6",
+        NO2: "4.7",
+        status: "對所有族群不健康"
+      }
     };
+    this.changeHandler = this.changeHandler.bind(this);
+    this.clickHandler = this.clickHandler.bind(this);
   }
-  // componentDidMount() {
+  componentDidMount() {
+    fetchJsonp(
+      "https://json2jsonp.com/?url=http://opendata2.epa.gov.tw/AQI.json"
+    )
+      .then(res => res.json())
+      .then(json => {
+        // const tmpJson = json.slice(0, 5);
+        const data = json.reduce((acc, record) => {
+          // console.log("original acc", JSON.stringify(acc));
+          const county = record.County;
+          // console.log("county", county);
+          const groupedRecords = acc[county] || [];
+          // console.log(
+          //   "original groupedRecords",
+          //   JSON.stringify(groupedRecords)
+          // );
+          groupedRecords.push(record);
+          // console.log(
+          //   "modified groupedRecords",
+          //   JSON.stringify(groupedRecords)
+          // );
+          acc[county] = groupedRecords;
+          // console.log("modified acc", JSON.stringify(acc));
+          return acc;
+        }, {});
+        this.setState({
+          data,
+          isLoading: false
+        });
+      })
+      .catch(e => console.log("catch", e));
+  }
+  changeHandler(e) {
+    this.setState({
+      cityName: e.target.value
+    });
+    const data = this.state.data;
+    const cityBeChoosed = data[e.target.value];
+    const siteName = cityBeChoosed.map(value => value.SiteName);
+    const aqi = cityBeChoosed.map(value => value.AQI);
+    const status = cityBeChoosed.map(value => value.Status);
+    const publishTime = cityBeChoosed[0].PublishTime;
+    const caseData = {
+      aqi: aqi,
+      status: status,
+      siteName: siteName,
+      cityBeChoosed: cityBeChoosed
+    };
 
-  //   fetch("http://opendata2.epa.gov.tw/AQI.json", {})
-  //     .then(res => res.json())
-  //     .then(json => {
-  //       console.log(json);
-  //       const records = json.result.records;
-  //       const result = records.reduce((acc, record) => {
-  //         const county = record.County;
-  //         const groupedRecords = acc[county] || [];
-  //         groupedRecords.push(record);
-  //         acc[county] = groupedRecords;
-  //         return acc;
-  //       }, {});
-  //       return result;
-  //     })
-  //     .then(data =>
-  //       this.setState({
-  //         data,
-  //         isLoading: false
-  //       })
-  //     )
-  //     .catch(e => console.log(e));
-
-  // }
-
+    this.setState({
+      publish: publishTime,
+      casedata: caseData
+    });
+  }
+  clickHandler(key) {
+    const cityBeChoosed = this.state.casedata.cityBeChoosed;
+    const statusdata = {
+      siteName: cityBeChoosed[key].SiteName,
+      aqi: cityBeChoosed[key].AQI,
+      O3: cityBeChoosed[key].O3,
+      PM10: cityBeChoosed[key].PM10,
+      PM25: cityBeChoosed[key]["PM2.5"],
+      CO: cityBeChoosed[key].CO,
+      SO2: cityBeChoosed[key].SO2,
+      NO2: cityBeChoosed[key].NO2,
+      status: cityBeChoosed[key].Status
+    };
+    this.setState({
+      statusData: statusdata
+    });
+  }
   render() {
     return (
       <div>
         <div className="container">
-          <Title cityName={Object.keys(this.state.data)} />
+          <Title
+            change={this.changeHandler}
+            cityName={Object.keys(this.state.data)}
+          />
           <StandardTable />
-          <City />
-          <Status />
+          <City cityName={this.state.cityName} publish={this.state.publish} />
+          <Status
+            statusData={this.state.statusData}
+            caseData={this.state.casedata}
+            click={this.clickHandler}
+          />
         </div>
         <div className="footer">
           <div className="datasrc">資料來源：行政院環境保護署</div>
@@ -59,15 +133,17 @@ class App extends React.Component {
 class Title extends React.Component {
   constructor(props) {
     super(props);
-    // this.clickHandler = this.clickHandler.bind(this);
   }
   render() {
     return (
       <div className="titleBox">
-        <div onClick={this.clickHandler.bind(this)} className="title">
-          空氣品質指標
-        </div>
-        <select className="location" id="location">
+        <div className="title">空氣品質指標</div>
+        <select
+          onChange={this.props.change}
+          value=""
+          className="location"
+          id="location"
+        >
           <option value="">請選擇地區</option>
           {this.props.cityName.map(city => (
             <option key={city} value={city}>
@@ -78,9 +154,6 @@ class Title extends React.Component {
       </div>
     );
   }
-  clickHandler() {
-    console.log(this);
-  }
 }
 
 class StandardTable extends React.Component {
@@ -88,34 +161,38 @@ class StandardTable extends React.Component {
     return (
       <div className="standardTableBox">
         <table className="standardTable">
-          <tr>
-            <th>0~50</th>
-            <th>51~100</th>
-            <th>101~150</th>
-            <th>151~200</th>
-            <th>201~300</th>
-            <th>301~400</th>
-          </tr>
-          <tr>
-            <td>
-              <div>良好</div>
-            </td>
-            <td>
-              <div>普通</div>
-            </td>
-            <td>
-              <div>對敏感族群不健康</div>
-            </td>
-            <td>
-              <div>對所有族群不健康</div>
-            </td>
-            <td>
-              <div>非常不健康</div>
-            </td>
-            <td>
-              <div>危害</div>
-            </td>
-          </tr>
+          <thead>
+            <tr>
+              <th>0~50</th>
+              <th>51~100</th>
+              <th>101~150</th>
+              <th>151~200</th>
+              <th>201~300</th>
+              <th>301~400</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <div>良好</div>
+              </td>
+              <td>
+                <div>普通</div>
+              </td>
+              <td>
+                <div>對敏感族群不健康</div>
+              </td>
+              <td>
+                <div>對所有族群不健康</div>
+              </td>
+              <td>
+                <div>非常不健康</div>
+              </td>
+              <td>
+                <div>危害</div>
+              </td>
+            </tr>
+          </tbody>
         </table>
       </div>
     );
@@ -123,51 +200,88 @@ class StandardTable extends React.Component {
 }
 
 class City extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
       <div className="cityBox">
-        <div className="cityName">高雄市</div>
+        <div className="cityName">{this.props.cityName}</div>
         <div className="line" />
-        <div className="update">2019-01-24 14:00 更新</div>
+        <div className="update">{this.props.publish} 更新</div>
       </div>
     );
   }
 }
 
 class Status extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
+    const { caseData } = this.props;
+    const { statusData } = this.props;
+    let statusBoxIndex = [];
+    for (let i = 0; i < caseData.siteName.length; i++) {
+      statusBoxIndex.push(i);
+    }
+    let color = "";
+    switch (statusData.status) {
+      case "良好":
+        color = "#95F084";
+        break;
+      case "普通":
+        color = "#FFE695";
+        break;
+      case "對敏感族群不健康":
+        color = "#FFAF6A";
+        break;
+      case "對所有族群不健康":
+        color = "#FF5757";
+        break;
+      case "非常不健康":
+        color = "#9777FF";
+        break;
+      case "危害":
+        color = "#AD1774";
+        break;
+      default:
+        break;
+    }
     return (
       <div className="status">
         <div className="box">
           <div className="chooseBox">
-            <div className="name">前金</div>
-            <div className="num">156</div>
+            <div className="name">{statusData.siteName}</div>
+            <div className="num" style={{ backgroundColor: color }}>
+              {statusData.aqi}
+            </div>
           </div>
           <div className="o3">
             <div className="chinese">臭氧</div>
             <div className="english">
               O<small>2</small>(ppb)
             </div>
-            <div className="value">82</div>
+            <div className="value">{statusData.O3}</div>
           </div>
           <div className="pm10">
             <div className="chinese">懸浮微粒</div>
             <div className="english">
               PM<small>10</small>(μg/m³)
             </div>
-            <div className="value">72</div>
+            <div className="value">{statusData.PM10}</div>
           </div>
           <div className="pm25">
             <div className="chinese">細懸浮微粒</div>
             <div className="english">
               PM<small>2.5</small>(μg/m³)
             </div>
-            <div className="value">67</div>
+            <div className="value">{statusData.PM25}</div>
           </div>
           <div className="co">
             <div className="chinese">一氧化碳</div>
             <div className="english">CO(ppm)</div>
-            <div className="value">0.49</div>
+            <div className="value">{statusData.CO}</div>
           </div>
 
           <div className="so2">
@@ -175,21 +289,73 @@ class Status extends React.Component {
             <div className="english">
               SO<small>2</small>(ppb)
             </div>
-            <div className="value">3.4</div>
+            <div className="value">{statusData.SO2}</div>
           </div>
           <div className="no2">
             <div className="chinese">二氧化氮</div>
             <div className="english">
               NO<small>2</small>(ppb)
             </div>
-            <div className="value">13</div>
+            <div className="value">{statusData.NO2}</div>
           </div>
         </div>
         <div className="caseContainer">
-          <div className="case">
-            <div className="name">前金</div>
-            <div className="num">156</div>
-          </div>
+          {statusBoxIndex.map(value => (
+            <StatusBox
+              key={value}
+              siteName={caseData.siteName[value]}
+              aqi={caseData.aqi[value]}
+              status={caseData.status[value]}
+              keyValue={value}
+              click={this.props.click}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+class StatusBox extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    let color = "";
+    switch (this.props.status) {
+      case "良好":
+        color = "#95F084";
+        break;
+      case "普通":
+        color = "#FFE695";
+        break;
+      case "對敏感族群不健康":
+        color = "#FFAF6A";
+        break;
+      case "對所有族群不健康":
+        color = "#FF5757";
+        break;
+      case "非常不健康":
+        color = "#9777FF";
+        break;
+      case "危害":
+        color = "#AD1774";
+        break;
+      default:
+        break;
+    }
+    return (
+      <div
+        className="case"
+        onClick={e => {
+          this.props.click(this.props.keyValue);
+        }}
+      >
+        <div className="name" value={this.props.siteName}>
+          {this.props.siteName}
+        </div>
+        <div className="num" style={{ backgroundColor: color }}>
+          {this.props.aqi}
         </div>
       </div>
     );
